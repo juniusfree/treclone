ced64b1676e2e1c6dd8512b74558905c729489d9
Junius Free B. Fontamillas <8164667+juniusfree@users.noreply.github.com>
2023-05-26 16:22:02 +0800

    ui: improve home page styling

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -19,35 +19,61 @@ const HomeComponent = () => {
         title,
       },
     });
+    setTitle("");
   };
   return (
-    <div>
-      {isCreating ? (
-        <div>
-          <input
-            type="text"
-            value={title}
-            placeholder="add board title"
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <div>
-            <button onClick={handleOnAdd}>Add Board</button>
-            <button className="w-4 h-4" onClick={toggleIsCreating}>
-              <XMarkIcon />
-            </button>
-          </div>
-        </div>
-      ) : (
-        <button onClick={toggleIsCreating}>Add a new board</button>
-      )}
-      <ul className="flex flex-col gap-4">
+    <div className="flex flex-col gap-8 p-8 w-full">
+      <div className="text-lg font-bold">Treclone</div>
+      <ul className="flex flex-wrap gap-4">
         {boards.map((board) => (
-          <li key={board.id}>
+          <li
+            key={board.id}
+            className="w-48 p-2 h-24 bg-gray-200 cursor-pointer rounded font-semibold hover:bg-gray-500"
+          >
             <Link href={`/board/${board.id}`}>
-              <div className="w-min-40 p-2 shadow">{board.title}</div>
+              <div className="h-full w-full overflow-hidden text-ellipsis whitespace-nowrap">
+                {board.title}
+              </div>
             </Link>
           </li>
         ))}
+        <li key="new-board" className="w-48 h-24">
+          {isCreating ? (
+            <div className="p-2 flex flex-col justify-between h-full bg-gray-200 rounded">
+              <input
+                autoFocus
+                className="w-full outline outline-gray-300 p-1 rounded text-sm"
+                type="text"
+                value={title}
+                placeholder="Enter board title"
+                onChange={(e) => setTitle(e.target.value)}
+                maxLength={20}
+              />
+              <div className="w-full flex items-center justify-between py-2">
+                <button
+                  onClick={handleOnAdd}
+                  className={`text-sm p-1 rounded bg-sky-700 text-white hover:bg-sky-900 disabled:bg-gray-300`}
+                  disabled={!title}
+                >
+                  Create
+                </button>
+                <button
+                  onClick={toggleIsCreating}
+                  className="text-sm p-1 rounded text-sky-700"
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div
+              onClick={toggleIsCreating}
+              className="cursor-pointer h-full w-full text-sm flex items-center justify-center p-2 bg-gray-200 hover:bg-gray-500 rounded"
+            >
+              Create a new board
+            </div>
+          )}
+        </li>
       </ul>
     </div>
   );
