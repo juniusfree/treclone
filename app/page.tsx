ef84d88143d17fe8e18ad10f2afbfa494627a5bf
Junius Free B. Fontamillas <8164667+juniusfree@users.noreply.github.com>
2023-05-30 15:58:56 +0800

    ui: hide on mobile devices

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
 import HomeComponent from "./components/home";
 
-const Home = () => <HomeComponent />;
+const Home = () => {
+  return (
+    <>
+      <div className="sm:hidden p-4 text-sm">
+        <p>
+          Sorry! This site is not available on mobile devices. Please use a
+          desktop or laptop computer to view this site.
+        </p>
+        <iframe
+          className="w-full h-auto"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          title="Rick Astley - Never Gonna Give You Up (Official Music Video)"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+      </div>
+      <div className="hidden sm:block">
+        <HomeComponent />
+      </div>
+    </>
+  );
+};
 
 export default Home;
